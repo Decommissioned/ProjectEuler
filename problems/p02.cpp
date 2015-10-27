67649cf98b11af0ec4d17a0ee04d8106d0e94535
Christian Gomes <christian.k.gomes@live.com>
2015-10-27 00:39:25 -0200

    Problem #002

---
+++ problems/p02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0;
+
+    int previous_term = 1;
+    int current_term = 1;
+
+    while (current_term < 4000000)
+    {
+        sum += current_term * !(current_term % 2);
+        int aux = previous_term;
+        previous_term = current_term;
+        current_term += aux;
+    }
+
+    std::cout << sum << std::endl;
+}
